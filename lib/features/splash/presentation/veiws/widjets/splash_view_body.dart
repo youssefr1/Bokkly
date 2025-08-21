39e485bfaf028ca05d915859994884c3171ea994
Youssefr1 <youssframa09@gmail.com>
2025-08-21 18:14:07 +0300

    first commit

---
+++ lib/features/splash/presentation/veiws/widjets/splash_view_body.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/cupertino.dart';
+
+class SplashViewBody extends StatelessWidget {
+  const SplashViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:get/get_navigation/src/root/get_material_app.dart';
+
+import 'features/splash/presentation/veiws/splash_view.dart';
+
+void main() {
+  runApp(const Bookly());
+}
+
+class Bookly extends StatelessWidget {
+  const Bookly({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const GetMaterialApp(
+      home: SplashView(),
+    );
+  }
+}
+
+
+
+
