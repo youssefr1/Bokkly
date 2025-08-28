fc3d9a850b81805a991481d01d5518de4448c64f
Youssefr1 <youssframa09@gmail.com>
2025-08-28 15:18:46 +0300

    create book details view

---
+++ lib/features/home/persentation/views/book_details_view.dart
@@ -0,0 +1,13 @@
+import 'package:bookly_app/features/home/persentation/views/widjets/book_details_veiw_body.dart';
+import 'package:flutter/material.dart';
+
+class BookDetailsView extends StatelessWidget {
+  const BookDetailsView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: BookDetailsVeiwBody(),
+    );
+  }
+}
