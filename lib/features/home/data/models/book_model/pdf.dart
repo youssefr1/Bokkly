c625a3a6bf3216cc85b5f910aba1b0d73d0d6876
Youssefr1 <youssframa09@gmail.com>
2025-09-02 01:29:18 +0300

    create home repositry

---
+++ lib/features/home/data/models/book_model/pdf.dart
@@ -0,0 +1,24 @@
+import 'package:equatable/equatable.dart';
+
+class Pdf extends Equatable {
+  final bool? isAvailable;
+  final String? acsTokenLink;
+
+  const Pdf({this.isAvailable, this.acsTokenLink});
+
+  factory Pdf.fromJson(Map<String, dynamic> json) => Pdf(
+    isAvailable: json['isAvailable'] as bool?,
+    acsTokenLink: json['acsTokenLink'] as String?,
+  );
+
+  Map<String, dynamic> toJson() => {
+    'isAvailable': isAvailable,
+    'acsTokenLink': acsTokenLink,
+  };
+
+  @override
+  bool get stringify => true;
+
+  @override
+  List<Object?> get props => [isAvailable, acsTokenLink];
+}
