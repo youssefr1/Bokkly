4f5b090765158e1d05074ccb15c3199d0c92837b
Youssefr1 <youssframa09@gmail.com>
2025-09-02 18:20:10 +0300

    create newset book methods

--- lib/features/home/persentation/view_models/newset books/newset_books_cubit.dart
+++ lib/features/home/persentation/view_models/newset books/newset_books_cubit.dart
@@ -1,4 +1,5 @@
 import 'package:bloc/bloc.dart';
+import 'package:bookly_app/features/home/data/repos/home_repo/home_repo.dart';
 import 'package:meta/meta.dart';
 
 import '../../../data/models/book_model/book_model.dart';
@@ -6,5 +7,19 @@ import '../../../data/models/book_model/book_model.dart';
 part 'newset_books_state.dart';
 
 class NewsetBooksCubit extends Cubit<NewsetBooksState> {
-  NewsetBooksCubit() : super(NewsetBooksInitial());
+  NewsetBooksCubit(this.homeRepo)
+      : super(NewsetBooksInitial());
+  final HomeRepo homeRepo;
+
+  Future<void> featchNewsetBooks() async {
+    emit(NewsetBooksloading());
+    var result = await homeRepo.fetchNewsetBooks();
+    result.fold(
+            (Failure) {
+          emit(NewsetBooksfailure(Failure.errMessage));
+        }, (Books) {
+      emit(NewsetBooksSuccess(Books));
+    }
+    );
+  }
 }
