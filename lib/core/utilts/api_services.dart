cc7de4d3a4a6cf391066aa135917fa44617f6894
Youssefr1 <youssframa09@gmail.com>
2025-09-02 16:36:46 +0300

    create api services

---
+++ lib/core/utilts/api_services.dart
@@ -0,0 +1,18 @@
+import 'package:bookly_app/features/home/data/models/book_model/book_model.dart';
+import 'package:dio/dio.dart' show Dio;
+
+class ApiServices{
+  final String _baseUrl ='https://www.googleapis.com/books/v1/';
+  final Dio dio ;
+  ApiServices(this.dio);
+
+
+
+  Future<Map<String, dynamic>> getBooks({required String endPoint}) async{
+  var responed = await  dio.get('$_baseUrl$endPoint');
+  return responed.data;
+  }
+
+
+
+}
